46434743a9680eb672955bd67ad3e90bb79e710e
Daniel Ossorio <daniel@ossorio.me>
2015-06-22 08:54:48 +0100

    Project created

---
+++ ios-clear-style-appTests/ios_clear_style_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_clear_style_appTests.swift
+//  ios-clear-style-appTests
+//
+//  Created by Daniel Ossorio on 22/06/2015.
+//  Copyright (c) 2015 Dan Ossorio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ios_clear_style_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
